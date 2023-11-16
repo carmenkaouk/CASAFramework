86e045cb2a72cae4a5f9b27df4b1e155658efc87
Fatima Karaki <Fatima.Karaki@bighand.com>
2023-11-16 16:14:34 +0200

    starting writing program.cs file

--- CASAFramework/Program.cs
+++ CASAFramework/Program.cs
@@ -1,3 +1,19 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using CASAFramework;
+using Microsoft.Extensions.Configuration;
+
+//configuration
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+//regester services 
+
+ServiceProvider serviceProvider = new ServiceProvider();
+// add and set needed stuff
+App app = new App();
+
+app.AddFileListener(); 
